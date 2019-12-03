ff4257e236acba84a4c2e692ee0de075f7783aa2
Kimi <tatyana.shvets@itechart-group.com>
2019-12-03 16:45:12 +0300

    Added sorting by gap

--- PeopleReport/Controllers/PersonPointsController.cs
+++ PeopleReport/Controllers/PersonPointsController.cs
@@ -16,13 +16,25 @@ namespace PeopleReport.Controllers
         {
             _personPointsRepository = repository;
         }
-        public ActionResult Index()
+        public ActionResult Index(int pointId = 0, int sortOrder= 0)
         {
             var list = _personPointsRepository.GetAllPersonPoints();
             var model = new List<PersonPointsWrapper>();
             foreach(var item in list)
             {
                 model.Add(new PersonPointsWrapper(item));
+            }
+
+            if (pointId != 0 && pointId < model[0].Points.Count)
+            {
+                if (sortOrder == 0)
+                {
+                    model = model.OrderBy(c => c.Points[pointId].Gap).ToList();
+                }
+                else
+                {
+                    model = model.OrderByDescending(c => c.Points[pointId].Gap).ToList();
+                }
             }
             return View(model);
         }
