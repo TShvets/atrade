5c5c0a84e9b941742e5408432498b1b38d2a9319
Kimi <tatyana.shvets@itechart-group.com>
2019-12-11 17:51:24 +0300

    Add Salary button and Controller

---
+++ PeopleReport/Controllers/SalaryController.cs
@@ -0,0 +1,19 @@
+﻿using PeopleReport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeopleReport.Controllers
+{
+    public class SalaryController : Controller
+    {
+        // GET: Salary
+        public ActionResult Index()
+        {
+            var salary = new Salary { Data = 14, TotalResult = 180 };
+            return View(salary);
+        }
+    }
+}
---
+++ PeopleReport/Models/Salary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeopleReport.Models
+{
+    public class Salary
+    {
+        public double TotalResult { get; set; }
+        public double Data { get; set; }
+    }
+}
